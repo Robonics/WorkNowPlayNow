d4f7a6bd4124b65644fa413f4b40408a16041d15
Robonics014 <ezj213@gmail.com>
2026-03-26 12:08:31 -0400

    Now uses index.html as the landing page. Basic header done

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.get("/", (req, res) => {
-	res.render("index.ejs", {});
+	res.redirect("/index.html");
 });
 
 app.listen( process.env.port || 8080, () => {
